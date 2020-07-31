85c7571779bd63cfef715f9ba5a8f8c3a864af76
Hardikgithub2020 <hardik.unity@gmail.com>
2020-07-31 16:36:42 -0400

    Debug the form component

--- sign_up_form/src/Components/Form.js
+++ sign_up_form/src/Components/Form.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 
 class Form extends Component{
@@ -16,20 +17,20 @@ class Form extends Component{
     handleUpdate = () => alert(`Hello, Your name is ${this.state.firstName} ${this.state.lastName}, Gender is ${this.state.gender}, and your occupation is ${this.state.occupation}` );
     render(){
         return(
-            <form onClick={this.handleUpdate}>
+            <form >
                 First Name<br/>
-                <input type='text' name='userName' value={this.state.firstName} onClick = {(e) => this.setState({ firstName : e.target.value})}/><br/>
+                <input type='text' name='userName' value={this.state.firstName} onChange = {(e) => this.setState({ firstName : e.target.value})}/><br/>
                 last Name<br/>
-                <input type='text' name='userName' value={this.state.lastName} onClick = {(e) => this.setState({ lastName : e.target.value})}/><br/>
+                <input type='text' name='userName' value={this.state.lastName} onChange = {(e) => this.setState({ lastName : e.target.value})}/><br/>
                 Password<br/>
-                <input type='password' name='password' value={this.state.password} onClick = {(e) => this.setState({ password : e.target.value})}/><br/>
+                <input type='password' name='password' value={this.state.password} onChange = {(e) => this.setState({ password : e.target.value})}/><br/>
                 Age<br/>
-                <input type='number' name='age' value={this.state.age} onClick = {(e) => this.setState({ age : e.target.value})}/><br/>
+                <input type='number' name='age' value={this.state.age} onChange = {(e) => this.setState({ age : e.target.value})}/><br/>
                 Gender<br/>
-                <input type='text' name='gender' value={this.state.gender} onClick = {(e) => this.setState({ gender : e.target.value})}/><br/>
+                <input type='text' name='gender' value={this.state.gender} onChange = {(e) => this.setState({ gender : e.target.value})}/><br/>
                 Occupation<br/>
-                <input type='text' name='age' value={this.state.occupation} onClick = {(e) => this.setState({occupation : e.target.value})}/><br/>
-                <button type='submit'>submit</button>
+                <input type='text' name='age' value={this.state.occupation} onChange = {(e) => this.setState({occupation : e.target.value})}/><br/>
+                <button type='submit' value='submit'onClick={this.handleUpdate}>submit</button>
 
                 
 
